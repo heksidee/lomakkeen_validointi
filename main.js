7ccb56229f05b79378ee9abe404880ec88bc2410
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-02-24 18:11:42 +0200

    tehty javascriptia ja muokattu css ja html

---
+++ main.js
@@ -0,0 +1,74 @@
+let painike = document.getElementById("painike");
+painike.addEventListener("click", yhdistettyFunktio);
+
+function yhdistettyFunktio(event) {
+    tarkastaId();
+    tarkastaSalasana();
+    tarkastaNimi();
+    tarkastaOsoite();
+    tarkastaMaa();
+    tarkastaZip();
+    tarkastaSposti();
+}
+
+function tarkastaId() {
+    let käytId = document.getElementById("kayttajaId").value;
+    document.getElementById("iiDee").innerHTML = ""
+    if (käytId.length < 6) {
+        document.getElementById("iiDee").innerHTML = "Käyttäjä ID pitää olla vähintään 6 merkkiä pitkä."
+    }
+}
+
+function tarkastaSalasana() {
+    let salaS = document.getElementById("salasana").value;
+    document.getElementById("ss").innerHTML = ""
+    let numerot = /[0-9]/
+    let erikois = /[!@£$€&%#]/
+    let isot = /[A-ZÖÄÅ]/
+    if (salaS.length < 6 || !numerot.test(salaS) || !erikois.test(salaS) || !isot.test(salaS)) {
+        document.getElementById("ss").innerHTML = "Salasanan tulee olla väh. 6 merkkiä, siinä tulee olla väh. 1 iso kirjain, 1 numero ja joku erikoismerkieistä '!@£$€&%#'"
+        }
+}
+
+function tarkastaNimi() {
+    let nimi = document.getElementById("nimi").value;
+    document.getElementById("name").innerHTML = ""
+    if (nimi.length < 1) {
+        document.getElementById("name").innerHTML = "Pakollinen kenttä, syötä nimi."
+    }
+}
+
+function tarkastaOsoite() {
+    let osoite = document.getElementById("osoite").value;
+    document.getElementById("address").innerHTML = ""
+    if (osoite.length < 1) {
+        document.getElementById("address").innerHTML = "Pakollinen kenttä, syötä osoite."
+    }
+}
+
+function tarkastaMaa() {
+    let maa = document.getElementById("maa").value;
+    document.getElementById("country").innerHTML = ""
+    if (maa === "(Valitse maa)") {
+        document.getElementById("country").innerHTML = "Pakollinen kenttä, valitse maa."
+    }
+}
+
+function tarkastaZip() {
+    let pnro = document.getElementById("postinumero").value;
+    document.getElementById("zip").innerHTML = ""
+    let numerot = /[0-9]/
+    if (pnro.length !== 5 || !numerot.test(pnro)) {
+        document.getElementById("zip").innerHTML = "Postinumerossa pitää olla 5 numeroa."
+    }
+}
+
+function tarkastaSposti() {
+    let maili = document.getElementById("sposti").value;
+    let sallittuMaili = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    document.getElementById("email").innerHTML = ""
+    if (!sallittuMaili.test(maili)) {
+        document.getElementById("email").innerHTML = "Anna kelvollinen sähköpostiosoite."
+    }
+}
+
